c1e6dfbaf5b95f2e98a44d9a422dde2a152a5b18
Rene Ruano <rene.ruano95@gmail.com>
2024-04-07 21:22:19 -0400

    added styles to login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,18 +1,23 @@
+import { Button } from "@/components/ui/button";
 import { login, signUp } from "./actions";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button className="bg-red-500" formAction={login}>
-        Log in
-      </button>
-      <button className="bg-green-500" formAction={signUp}>
-        Sign up
-      </button>
-    </form>
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <form className="flex flex-col gap-2">
+        <Label htmlFor="email">Email:</Label>
+        <Input id="email" name="email" type="email" required />
+        <Label htmlFor="password">Password:</Label>
+        <Input id="password" name="password" type="password" required />
+        <Button variant={"default"} formAction={login}>
+          Log in
+        </Button>
+        <Button variant={"secondary"} formAction={signUp}>
+          Sign up
+        </Button>
+      </form>
+    </div>
   );
 }
